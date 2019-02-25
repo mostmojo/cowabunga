0518f9017fa9e0d729a1e8f06a423b02da69083b
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-02-25 18:25:04 +0000

    fixed routes and nested/unested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :surf_companies, only: [:index, :show]
-  resources :bookings
+  resources :surf_companies, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, except: [:new, :create]
+
 end
 # when we add reviews, then we need to nest them because they relate to a specific surf company
