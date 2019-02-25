419e0593464b4fbf9cffb0a28367d7f054c09a59
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-02-25 15:20:07 +0000

    created models

---
+++ app/models/surf_company.rb
@@ -0,0 +1,2 @@
+class SurfCompany < ApplicationRecord
+end
---
+++ db/migrate/20190225151238_create_surf_companies.rb
@@ -0,0 +1,10 @@
+class CreateSurfCompanies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :surf_companies do |t|
+      t.string :name
+      t.text :address
+
+      t.timestamps
+    end
+  end
+end
