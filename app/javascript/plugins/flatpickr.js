5cca9db48e74a9edb2c20c24533a89b51d582bee
Justas Jaskunas <jaskunas.justas@gmail.com>
2019-02-28 16:09:36 +0000

    booking form

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,18 @@
+import flatpickr from "flatpickr"
+import $ from 'jquery'
+import "flatpickr/dist/themes/dark.css"
+import minMaxTimePlugin from 'flatpickr/dist/plugins/minMaxTimePlugin'
+$('.dater').flatpickr({
+  enableTime: true,
+  minDate: "2019",
+  plugins: [
+    new minMaxTimePlugin({
+      table: {
+        "2019-03-01": {
+          minTime: "06:00",
+          maxTime: "18:00"
+      }
+    }
+  })
+    ]
+});
