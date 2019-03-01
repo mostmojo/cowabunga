e3cb5a69d43266220bd7453e7c5aeda06e09bf1d
Tasneem Karwa <tasneemkarwa@gmail.com>
2019-03-01 12:12:11 +0000

    add columns to surf companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_105214) do
+ActiveRecord::Schema.define(version: 2019_03_01_105006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2019_02_27_105214) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
+    t.string "facilities"
+    t.string "number"
+    t.string "email"
   end
 
   create_table "users", force: :cascade do |t|
